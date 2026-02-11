b713c3078540c2cc21a1c790eae8e4e4a1ecab20
Sibar Al-ani <sibar.alani@gmail.com>
2026-02-11 10:51:36 +0100

    Blog solution with TS done

--- 21-lecture-prerecorder/blog-solution-part3-typescript/ts/api.ts
+++ 21-lecture-prerecorder/blog-solution-part3-typescript/ts/api.ts
@@ -1,18 +1,35 @@
 
+export interface PostOverview {
+    id: number;
+    title: string;
+    date: string;
+    text: string;
+}
+
+export interface PostDetails extends PostOverview{
+    // id: number;
+    // title: string;
+    // date: string;
+    // text: string;
+    image_url: string;
+    tags: string[];
+}
+
 
 // Prompt 1: create an async function that fetcehs blog posts from: https://codexplained.se/api/blog_json.php
-export async function fetchBlogPosts() {
+export async function fetchBlogPosts(): Promise<PostOverview[]> {
     try {
         const response = await fetch('https://codexplained.se/api/blog_json.php');
         return await response.json();
     } catch (error) {
         console.error('Error fetching blog posts:', error);
+        throw error;
     }
 }
 
 
 // Promot 2: fetch from https://codexplained.se/api/post_json.php?post=1
-export async function fetchBlogPost() {
+export async function fetchBlogPost(): Promise<PostDetails> {
     try {
         const urlParams = new URLSearchParams(window.location.search);
         const postId = urlParams.get('id');
@@ -21,5 +38,6 @@ export async function fetchBlogPost() {
 
     } catch (error) {
         console.error('Error fetching blog post:', error);
+        throw error;
     }
 }
